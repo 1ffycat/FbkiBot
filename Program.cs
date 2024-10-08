91a695de220cd7e27f4b75ea0b4b97a7ccacbcd0
Vsevolod Levitan <levvsev@gmail.com>
2024-10-08 15:28:05 +0800

    Added empty app host

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+var app = builder.Build();
+app.Run();
