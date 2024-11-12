cba99bb83ad0a1500015b68f99e6633bcf1ac7f8
Vsevolod Levitan <levvsev@gmail.com>
2024-11-12 12:37:00 +0800

    Added middleware implementation

---
+++ FbkiBot/Middleware/IBotMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using FbkiBot.Models;
+
+namespace FbkiBot.Middleware;
+
+/// <summary>
+/// ПО промежуточного слоя для обработки событий бота
+/// </summary>
+public interface IBotMiddleware
+{
+    /// <summary>
+    /// Выполнить обработку и, опционально, продолжить выполнение пайплайна
+    /// </summary>
+    /// <param name="context">Контекст события</param>
+    /// <param name="next">Следующий middleware</param>
+    Task InvokeAsync(UpdateContext context, BotMiddlewareDelegate next);
+}
+
+/// <summary>
+/// Делегат вызова middleware
+/// </summary>
+public delegate Task BotMiddlewareDelegate(UpdateContext context);
---
+++ FbkiBot/Models/UpdateContext.cs
@@ -0,0 +1,34 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace FbkiBot.Models;
+
+/// <summary>
+/// Контекст события Telegram бота
+/// </summary>
+/// <param name="update">Само событие</param>
+/// <param name="client">Клиент Telegram-бота</param>
+/// <param name="services">Сервисы приложения</param>
+/// <param name="ct">Токен для отмены выполнения</param>
+public class UpdateContext(Update update, ITelegramBotClient client, IServiceProvider services, CancellationToken ct)
+{
+    /// <summary>
+    /// Событие Telegram-бота
+    /// </summary>
+    public Update Update { get; set; } = update;
+
+    /// <summary>
+    /// Клиент Telegram-бота, из которого получено событие
+    /// </summary>
+    public ITelegramBotClient Client { get; set; } = client;
+
+    /// <summary>
+    /// Провайдер сервисов приложения
+    /// </summary>
+    public IServiceProvider Services { get; set; } = services;
+
+    /// <summary>
+    /// Токен для отмены выполнения
+    /// </summary>
+    public CancellationToken CancellationToken { get; set; } = ct;
+}
